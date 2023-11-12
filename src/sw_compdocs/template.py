9a91e93c4790174b498c2a62d1b40fbd2cf7230c
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2023-11-12 14:23:23 +0900

    Add sw_compdocs.template.TemplateRenderer

--- src/sw_compdocs/template.py
+++ src/sw_compdocs/template.py
@@ -2,6 +2,40 @@ import collections.abc
 import re
 
 
+class TemplateRenderer:
+    def __init__(self, mapping):
+        if not isinstance(mapping, collections.abc.Mapping):
+            raise TemplateMappingError(
+                f"template mapping must be mapping, not '{type(mapping).__name__}'"
+            )
+
+        self._d = {}
+        for key, val in mapping.items():
+            if type(key) is not str:
+                raise TemplateMappingError(
+                    f"template mapping entry key must be str, not '{type(key).__name__}'"
+                )
+            if type(val) is not str:
+                raise TemplateMappingError(
+                    f"template mapping entry value must be str, not '{type(val).__name__}'"
+                )
+            self._d[key] = val
+
+    def render(self, s):
+        if type(s) is not str:
+            raise TypeError
+
+        def repl(match):
+            key = match["key"]
+            try:
+                val = self._d[key]
+            except KeyError:
+                raise TemplateKeyError(key)
+            return val
+
+        return re.sub(r"(?s:\$\[(?P<key>.*?)\])", repl, s)
+
+
 def as_mapping(v):
     if not isinstance(v, collections.abc.Mapping):
         raise TypeError
@@ -34,6 +68,10 @@ def substitute(s, mapping):
         return val
 
     return re.sub(r"(?s:\$\[(?P<key>.*?)\])", repl, s)
+
+
+class TemplateMappingError(Exception):
+    pass
 
 
 class TemplateKeyError(Exception):
--- src/sw_compdocs_tests/test_template.py
+++ src/sw_compdocs_tests/test_template.py
@@ -1,5 +1,97 @@
+import collections
 import sw_compdocs.template
 import unittest
+
+
+class TestTemplateRendererInit(unittest.TestCase):
+    def test_validate_pass(self):
+        for mapping in [
+            {"a": ""},
+            {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_": ""},
+            {"a": "x"},
+            {"action_down": "s"},
+            {"a": "日本語もOK"},
+            {"a": "", "b": ""},
+            {"a": "x", "b": "y"},
+        ]:
+            with self.subTest(mapping=mapping):
+                ren = sw_compdocs.template.TemplateRenderer(mapping)
+                self.assertEqual(ren._d, mapping)
+                self.assertIsNot(ren._d, mapping)
+
+    def test_validate_error(self):
+        tt = collections.namedtuple("tt", ("input_mapping", "want_exc_args"))
+
+        for tc in [
+            tt(
+                input_mapping=[],
+                want_exc_args=("template mapping must be mapping, not 'list'",),
+            ),
+            tt(
+                input_mapping={0: ""},
+                want_exc_args=("template mapping entry key must be str, not 'int'",),
+            ),
+            tt(
+                input_mapping={"": 0},
+                want_exc_args=("template mapping entry value must be str, not 'int'",),
+            ),
+            tt(
+                input_mapping={"a": "", 0: ""},
+                want_exc_args=("template mapping entry key must be str, not 'int'",),
+            ),
+            tt(
+                input_mapping={"a": "", "": 0},
+                want_exc_args=("template mapping entry value must be str, not 'int'",),
+            ),
+        ]:
+            with self.subTest(tc=tc):
+                with self.assertRaises(sw_compdocs.template.TemplateMappingError) as cm:
+                    sw_compdocs.template.TemplateRenderer(tc.input_mapping)
+                self.assertEqual(cm.exception.args, tc.want_exc_args)
+
+
+class TestTemplateRendererRender(unittest.TestCase):
+    def test_type_error(self):
+        ren = sw_compdocs.template.TemplateRenderer({})
+        with self.assertRaises(TypeError):
+            ren.render(0)
+
+    def test_key_error(self):
+        tt = collections.namedtuple("tt", ("input_mapping", "input_s", "want_key"))
+
+        for tc in [
+            tt(input_mapping={"bar": "baz"}, input_s="$[foo]", want_key="foo"),
+            tt(input_mapping={}, input_s="$[foo\n]", want_key="foo\n"),
+            tt(input_mapping={"foo": "bar"}, input_s="$[foo] $[baz]", want_key="baz"),
+        ]:
+            with self.subTest(tc=tc):
+                ren = sw_compdocs.template.TemplateRenderer(tc.input_mapping)
+                with self.assertRaises(sw_compdocs.template.TemplateKeyError) as cm:
+                    ren.render(tc.input_s)
+                self.assertEqual(cm.exception.key, tc.want_key)
+
+    def test_pass(self):
+        tt = collections.namedtuple("tt", ("input_mapping", "input_s", "want_s"))
+
+        for tc in [
+            tt(input_mapping={}, input_s="", want_s=""),
+            tt(input_mapping={"foo": "bar"}, input_s="foo", want_s="foo"),
+            tt(input_mapping={"foo": "bar"}, input_s="$foo", want_s="$foo"),
+            tt(input_mapping={"foo": "bar"}, input_s="${foo}", want_s="${foo}"),
+            tt(input_mapping={"foo": "bar"}, input_s="$[foo", want_s="$[foo"),
+            tt(input_mapping={"foo": "bar"}, input_s="$[foo]", want_s="bar"),
+            tt(input_mapping={"foo": "$[foo]"}, input_s="$[foo]", want_s="$[foo]"),
+            tt(input_mapping={"foo": r"\n"}, input_s="$[foo]", want_s=r"\n"),
+            tt(
+                input_mapping={"foo": "bar", "baz": "qux"},
+                input_s="foo $[foo] baz $[baz]",
+                want_s="foo bar baz qux",
+            ),
+        ]:
+            with self.subTest(tc=tc):
+                ren = sw_compdocs.template.TemplateRenderer(tc.input_mapping)
+                got_s = ren.render(tc.input_s)
+                self.assertEqual(got_s, tc.want_s)
 
 
 class TestAsMapping(unittest.TestCase):
