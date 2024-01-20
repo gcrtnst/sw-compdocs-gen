e966d6b905266ec03a7ea39b2f7487686ab7158e
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2024-01-20 23:57:29 +0900

    Add sw_compdocs.resource

---
+++ src/sw_compdocs/resource.py
@@ -0,0 +1,42 @@
+import re
+
+
+def format_toml_key(key):
+    if type(key) is not str:
+        raise TypeError
+
+    if re.search(r"\A[A-Za-z0-9_\-]+\Z", key) is not None:
+        return key
+    return format_toml_string(key)
+
+
+def format_toml_string(s):
+    if type(s) is not str:
+        raise TypeError
+
+    esc_set = frozenset(chr(i) for i in range(0x00, 0x20)) | {'"', "\\", "\x7F"}
+    esc_dict = {
+        "\b": r"\b",
+        "\t": r"\t",
+        "\n": r"\n",
+        "\f": r"\f",
+        "\r": r"\r",
+        '"': r"\"",
+        "\\": r"\\",
+    }
+
+    l = []
+    for c in s:
+        if c not in esc_set:
+            l.append(c)
+            continue
+        if c in esc_dict:
+            l.append(esc_dict[c])
+            continue
+
+        u = ord(c)
+        if u <= 0xFFFF:
+            l.append(f"\\u{u:04X}")
+            continue
+        l.append(f"\\U{u:08X}")
+    return '"' + "".join(l) + '"'
