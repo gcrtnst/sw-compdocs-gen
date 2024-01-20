ea9a0c5540cf0f3c9db70ac4cf39c2f5aee9d6f8
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2024-01-21 00:17:57 +0900

    Add type annotations to sw_compdocs.validator

--- src/sw_compdocs/validator.py
+++ src/sw_compdocs/validator.py
@@ -1,5 +1,9 @@
 import os
+import typing
 
 
-def is_pathlike(v):
+StrOrBytesPath: typing.TypeAlias = str | bytes | os.PathLike[str] | os.PathLike[bytes]
+
+
+def is_pathlike(v: object) -> typing.TypeGuard[StrOrBytesPath]:
     return type(v) is str or type(v) is bytes or isinstance(v, os.PathLike)
