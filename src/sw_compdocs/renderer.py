80b312a2f0b0e96c741c794f48814f36ada32fc0
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2024-01-25 23:46:23 +0900

    Add type annotations to sw_compdocs.renderer

--- src/sw_compdocs/renderer.py
+++ src/sw_compdocs/renderer.py
@@ -1,14 +1,16 @@
+import typing
+
 from . import document
 
 
-def render_markdown(doc):
+def render_markdown(doc: document.Document) -> str:
     if not isinstance(doc, document.Document):
         raise TypeError
 
     return "\n".join(render_markdown_block(blk) for blk in doc)
 
 
-def render_markdown_block(blk):
+def render_markdown_block(blk: document.Block) -> str:
     if not isinstance(blk, document.Block):
         raise TypeError
 
@@ -23,26 +25,26 @@ def render_markdown_block(blk):
     raise Exception
 
 
-def render_markdown_heading(head):
+def render_markdown_heading(head: document.Heading) -> str:
     if not isinstance(head, document.Heading):
         raise TypeError
     return "#" * head.level + " " + head.text + "\n"
 
 
-def render_markdown_paragraph(para):
+def render_markdown_paragraph(para: document.Paragraph) -> str:
     if not isinstance(para, document.Paragraph):
         raise TypeError
     return para.text + "\n"
 
 
-def render_markdown_table(tbl):
+def render_markdown_table(tbl: document.Table) -> str:
     if not isinstance(tbl, document.Table):
         raise TypeError
 
     return render_markdown_table_data(tbl.data)
 
 
-def render_markdown_table_data(data):
+def render_markdown_table_data(data: document.TableData) -> str:
     if not isinstance(data, document.TableData):
         raise TypeError
 
@@ -53,14 +55,14 @@ def render_markdown_table_data(data):
     )
 
 
-def render_markdown_table_data_row(row):
+def render_markdown_table_data_row(row: document.TableDataRow) -> str:
     if not isinstance(row, document.TableDataRow):
         raise TypeError
 
     return "| " + " | ".join(row) + " |\n"
 
 
-def render_markdown_table_data_delimiter(n):
+def render_markdown_table_data_delimiter(n: int) -> str:
     if type(n) is not int:
         raise TypeError
     if n <= 0:
@@ -69,7 +71,7 @@ def render_markdown_table_data_delimiter(n):
     return "| " + "--- | " * (n - 1) + "--- |\n"
 
 
-def render_markdown_callout(callout):
+def render_markdown_callout(callout: document.Callout) -> str:
     if not isinstance(callout, document.Callout):
         raise TypeError
 
@@ -78,7 +80,7 @@ def render_markdown_callout(callout):
     elif callout.kind is document.CalloutKind.WARNING:
         kind = "WARNING"
     else:
-        raise Exception
+        typing.assert_never(callout.kind)
 
     l = [f"[!{kind}]"]
     l.extend(callout.text.split("\n"))
