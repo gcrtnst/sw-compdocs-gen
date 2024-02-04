be6d5dcee2cf5b9e8c56c4e0b4b882cadde3be7f
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2024-02-04 18:32:12 +0900

    Remove sw_compdocs.validator

--- src/sw_compdocs/validator.py
+++
@@ -1,8 +0,0 @@
-import os
-import typing
-
-from . import _types
-
-
-def is_pathlike(v: object) -> typing.TypeGuard[_types.StrOrBytesPath]:
-    return type(v) is str or type(v) is bytes or isinstance(v, os.PathLike)
