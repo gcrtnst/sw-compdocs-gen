891c87212c98cb34e6a48c5083b4ac21b3663026
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2024-04-30 00:08:20 +0900

    Add sw_compdocs.language.Language.translate

--- src/sw_compdocs/language.py
+++ src/sw_compdocs/language.py
@@ -149,3 +149,10 @@ class Language(container.Sequence[Translation]):
         if len(trans_list) <= 0:
             raise LanguageFindEnError(en)
         return trans_list[0]
+
+    def translate(self, text: Text) -> str:
+        if text.id is not None:
+            trans = self.find_id(text.id)
+        else:
+            trans = self.find_en(text.en)
+        return trans.local
