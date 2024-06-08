6bae394de274bd27a7909bc79681400b6f104437
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2024-06-09 00:15:13 +0900

    Handle non-zero exit status in run_all.py

--- run_all.py
+++ run_all.py
@@ -9,7 +9,9 @@ def main() -> None:
     repo_dir = pathlib.Path(__file__).parent
     for script_name in ["run_test.py", "run_update.py"]:
         script_file = pathlib.Path(repo_dir, script_name)
-        subprocess.run([sys.executable, script_file])
+        result = subprocess.run([sys.executable, script_file])
+        if result.returncode != 0:
+            sys.exit(result.returncode)
 
 
 if __name__ == "__main__":
